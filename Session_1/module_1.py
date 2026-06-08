6e1bea8ffbb9ef399f8b85d5c2ee0aab73efc24a
dfsdf fsdf <stanislavmdimitrov03@gmail.com>
2026-06-08 20:20:55 +0300

    Solution

--- Session_1/module_1.py
+++ Session_1/module_1.py
@@ -2,35 +2,48 @@ from typing import List
 
 
 def task_1(array: List[int], target: int) -> List[int]:
-    """
-    Write your code below
-    """
-    pass
+    seen = {}
+    for num in array:
+        complement = target - num
+        if complement in seen:
+            return [complement, num]
+        seen[num] = True
+    return []
 
 
 def task_2(number: int) -> int:
-    """
-    Write your code below
-    """
-    pass
+    result = 0
+    number = abs(number)
+    while number > 0:
+        result = result * 10 + number % 10
+        number //= 10
+    return result
 
 
 def task_3(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    for i in range(len(array)):
+        val = abs(array[i]) - 1
+        if array[val] < 0:
+            return abs(array[i])
+        array[val] = -array[val]
+    return -1
 
 
 def task_4(string: str) -> int:
-    """
-    Write your code below
-    """
-    pass
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(string)):
+        if i + 1 < len(string) and roman[string[i]] < roman[string[i + 1]]:
+            result -= roman[string[i]]
+        else:
+            result += roman[string[i]]
+    return result
 
 
 def task_5(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    smallest = array[0]
+    for num in array[1:]:
+        if num < smallest:
+            smallest = num
+    return smallest
